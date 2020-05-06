0cd90a369a4b99572277778610f5de88b3c15100
Keaton Blomquist <Keaton.blomquist@gmail.com>
2020-05-06 16:09:04 -0600

    Alpha 0.0.1

--- RyzenSmartTuning/RyzenSmartTuning.cpp
+++ RyzenSmartTuning/RyzenSmartTuning.cpp
@@ -9,13 +9,16 @@
 #include <string.h>
 #include <thread>
 #include <cstdint>
+#include <fstream>
+#include<sstream> 
+using namespace std;
 #ifdef __linux__
 #include <unistd.h>
 #endif
 
 
 
-
+//might need 
 #include <AMDTPowerProfileApi.h>
 
 #include <ryzenadj.h>
@@ -28,59 +31,110 @@
 
 int main()
 {
-    fprintf(stdout, "Enter In 0 for any setting you do not want to be applied\n");
+    string currentSetting;
+    ifstream inputSettingsFile;
+    ofstream createSettingsFile;
+    
     uint32_t userSettings[22];
     uint32_t temp;
-    const char* description[22] = { "0. STAPM Limit (W)", "1. Fast Limit (W)", "2. Slow Limit (W)",  "3. Slow Time (S)",
-    "4. STAPM Time (S)", "5. Tctl Temperature (Degrees C)", "6. VRM Current Limit (A)", "7. VRM SoC Current Limit (mA)",
-        "8. VRM Maximum Current Limit (A)", "9. VRM SoC Maximum Current Limit (A)", "10. PSI0 Current Limit (mA)", "11. PSI0 SoC Current Limit(mA)",
-        "12. Maximum SoC Clock Frequency(MHz)", "13. Minimum SoC Clock Frequency(MHz)", "14. Maximum Transmission(CPU - GPU) Frequency(MHz)", "15. Minimum Transmission(CPU - GPU) Frequency(MHz)",
-        "16. Maximum Video Core Next(VCE - Video Coding Engine) (Value)", "17. Minimum Video Core Next(VCE - Video Coding Engine) (Value)", "18. Maximum Data Launch Clock(Value)", "19. Minimum Data Launch Clock(Value)",
-        "20. Maximum GFX Clock(Value)", "21. Minimum GFX Clock(Value)" };
-    for (int i = 0; i < 22; i++)
+    //opening out setttings file
+    inputSettingsFile.open("settings.txt");
+
+    if (inputSettingsFile.is_open())
     {
-        if ((i < 9 || i > 19) && i != 3 && i != 4 && i != 6 && i != 7)
+        //reading in file into userSettings if it exists
+        for (int i = 0; i < 22; i++)
         {
-        
-            fprintf(stdout, "Enter In %s: ", description[i]);
-            std::cin >> temp;
-            if (i == 0 || i == 1 || i == 2 || i == 6 || i == 7 || i == 8 || i == 9)
-            {
-                temp *= 1000;
-            }
+            getline(inputSettingsFile, currentSetting);
+            std::string::size_type sz;   // alias of size_t
+            temp = std::stoi(currentSetting, &sz);
+
+            //std::cout << currentSetting << std::endl;
+            std::cout << "Loaded: " << temp << std::endl;
             userSettings[i] = temp;
         }
-        else
-        {
-            userSettings[i] = 0;
-        }
+
+
+        inputSettingsFile.close();
     }
-    RyzenSmartTuning ryzenST(true, 100, true, userSettings);
-    system("cls");
+    else
+    {
+        inputSettingsFile.close();
+        createSettingsFile.open("settings.txt");
+        if (createSettingsFile.is_open())
+        {
+            cout << "Created New Setings File\n";
+
+        }
 
 
 
 
-    //ryzenST.startUProf();
+
+        //user input
+        fprintf(stdout, "Enter In 0 for any setting you do not want to be applied\n");
+        //all of the descriptions here with the index of where they go when the go off to RST
+        const char* description[22] = { "0. STAPM Limit (W)", "1. Fast Limit (W)", "2. Slow Limit (W)",  "3. Slow Time (Package Power Tracking (PPT) - Slow period) (S)",
+        "4. STAPM Time (CPU Boost Period) (S)", "5. Tctl Temperature (Degrees C)", "6. VRM Current Limit (A)", "7. VRM SoC Current Limit (mA)",
+            "8. VRM Maximum Current Limit (A)", "9. VRM SoC Maximum Current Limit (A)", "10. PSI0 Current Limit (mA)", "11. PSI0 SoC Current Limit(mA)",
+            "12. Maximum SoC Clock Frequency(MHz)", "13. Minimum SoC Clock Frequency(MHz)", "14. Maximum Transmission(Infinity Fabric) Frequency(MHz)", "15. Minimum Transmission(Infinity Fabric) Frequency(MHz)",
+            "16. Maximum Video Core Next(VCE - Video Coding Engine) (Value)", "17. Minimum Video Core Next(VCE - Video Coding Engine) (Value)", "18. Maximum Data Launch Clock(Value)", "19. Minimum Data Launch Clock(Value)",
+            "20. Maximum GFX Clock(Value)", "21. Minimum GFX Clock(Value)" };
+        //removing extra options for now
+        for (int i = 0; i < 22; i++)
+        {
+            if (((i < 9 || i > 19   ) && i != 3 && i != 6 && i != 7) || i == 10 || i == 14 || i == 15)
+            {
+                
+
+
+                fprintf(stdout, "Enter In %s: ", description[i]);
+                std::cin >> temp;
+                //miltiplying limits that need to be
+                if (i == 0 || i == 1 || i == 2 || i == 6 || i == 7 || i == 8 || i == 9)
+                {
+                    temp *= 1000;
+                }
+                userSettings[i] = temp;
+
+                stringstream ss;
+                ss << temp;
+                ss >> currentSetting;
+
+                createSettingsFile << currentSetting << '\n';
+            }
+            else
+            {
+                userSettings[i] = 0;
+
+                createSettingsFile << "0\n";
+            }
+            
+        }
+        createSettingsFile.close();
+    }
+
+
+    //user input
+   
+    
+    //std::system("pause");
+    
+    //sending all the data off to RST
+    RyzenSmartTuning ryzenST(true, 200, true, userSettings);
+    std::system("cls");
+
+
+
+
+    //initing RyzenAdj
     ryzenST.initRyzenAdj();
     if (ryzenST.ryzenAccess == NULL) {
         printf("Unable to init ryzenadj, check permission\n");
-        system("pause");
+        std::system("pause");
     }
+    //starting RST
     ryzenST.startRST();
-    
-    /*
-    UProfData test;
-    system("cls");
-    test.initalizeUProf(false, false, true);
-    test.startProf();
-    
-    while (true)
-    {
-        test.update();
-    }
-    test.stopProf();
-    system("pause");
-    */
+
     return 0;
 }
